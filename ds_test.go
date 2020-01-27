d7ada181f8bbc570b6664f556fb8df233850363b
sinmetal <metal.tie@gmail.com>
2020-01-27 20:08:14 +0900

    add test

---
+++ ds_test.go
@@ -0,0 +1,49 @@
+package ds_test
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	cds "cloud.google.com/go/datastore"
+	"go.mercari.io/datastore"
+	"go.mercari.io/datastore/clouddatastore"
+)
+
+type Hoge struct {
+	ID   string `datastore:"-"`
+	Body string
+}
+
+func TestGetMulti(t *testing.T) {
+	if err := os.Setenv("DATASTORE_EMULATOR_HOST", "localhost:8081"); err != nil {
+		t.Fatal(err)
+	}
+
+	ctx := context.Background()
+	client, err := cds.NewClient(ctx, "hoge")
+	if err != nil {
+		t.Fatal(err)
+	}
+	ds, err := clouddatastore.FromClient(ctx, client)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	keys := []datastore.Key{ds.NameKey("Hoge", "hoge1", nil)}
+	vs := []*Hoge{
+		{
+			Body: "Hogeeeee",
+		},
+	}
+
+	_, err = ds.PutMulti(ctx, keys, vs)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var rvs []*Hoge
+	if err := ds.GetMulti(ctx, keys, &rvs); err != nil {
+		t.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/sinmetal/dstest
+
+go 1.12
+
+require (
+	cloud.google.com/go/datastore v1.0.0
+	go.mercari.io/datastore v1.6.1
+)
